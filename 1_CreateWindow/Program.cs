cac8e806e09a7a03310dffded950e39ea0e7eb40
Chen Tang <1601865659@qq.com>
2024-06-29 09:29:26 +0800

    添加项目文件。

---
+++ 1_CreateWindow/Program.cs
@@ -0,0 +1,45 @@
+﻿using Silk.NET.Windowing;
+
+namespace _1_CreateWindow
+{
+    internal class Program
+    {
+        private static IWindow _window;
+        static void Main(string[] args)
+        {
+            _window = Window.Create(WindowOptions.Default with{
+                API = GraphicsAPI.None,
+                WindowBorder = WindowBorder.Fixed
+            });
+
+            _window.Initialize();
+            _window.Center();
+            _window.Load += WindowLoad;
+            _window.Render += WindowRender;
+            _window.Update += WindowOnUpdate;
+            _window.Closing += WindowClosing;
+
+            _window.Run();
+        }
+
+        private static void WindowOnUpdate(double obj)
+        {
+            Console.WriteLine("WindowOnUpdate");
+        }
+
+        private static void WindowClosing()
+        {
+            Console.WriteLine("Closing");
+        }
+
+        private static void WindowRender(double obj)
+        {
+            Console.WriteLine("window_Render");
+        }
+
+        private static void WindowLoad()
+        {
+            Console.WriteLine("window_Load");
+        }
+    }
+}
